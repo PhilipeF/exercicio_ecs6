37d9810d7166e1e397f48cd6636fae648e6586e1
PhilipeF <philipesferreiraa@gmail.com>
2024-07-10 15:24:12 -0300

    Feat: Exercício ECS6 - OK

---
+++ exercicio_ecs6.js
@@ -0,0 +1,14 @@
+const alunos = [
+    { nome: "Philipe", nota: 8.6 }, 
+    { nome: "Alvaro", nota: 4.5},
+    { nome: "Renan" , nota: 7.0},
+    { nome: "Maria", nota: 6.0},
+    { nome: "Ana Clara", nota: 4.0},
+    { nome: "Michle", nota: 9.0}
+];
+
+const filtrarAlunos = (item)=>  item.nota >= 6
+
+const alunosAprovados = alunos.filter(filtrarAlunos)
+
+console.log(alunosAprovados)
